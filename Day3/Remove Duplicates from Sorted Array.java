c18ab5035eb8503bd56adfe8bb8ac30c76b8964e
Aparna Kochar <53809016+aparnakochar@users.noreply.github.com>
2022-03-31 14:28:37 +0530

    Day3 Challenge

---
+++ Day3/Remove Duplicates from Sorted Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+      int n=nums.length;
+        int j=1;
+        for(int i=0;i<n-1;i++){
+            if(nums[i]!=nums[i+1]){
+                nums[j]=nums[i+1];
+                j++;
+            }
+        }
+        return j;
+    }
+}
